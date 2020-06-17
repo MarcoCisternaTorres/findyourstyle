d64635aa10e54909442a4b4f15a80d95b44c783d
Marco-Cisterna <marcocisterna0604@outlook.es>
2020-06-16 21:20:05 -0400

    permiso de camara funciona

--- app/src/main/java/com/example/findyourstyle/Fragments/PerfilFragment.java
+++ app/src/main/java/com/example/findyourstyle/Fragments/PerfilFragment.java
@@ -1,27 +1,55 @@
 package com.example.findyourstyle.Fragments;
 
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.findyourstyle.Activities.MainActivity;
+import com.example.findyourstyle.BuildConfig;
 import com.example.findyourstyle.R;
+
+import java.io.File;
+import java.util.Objects;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PerfilFragment extends Fragment {
 
-    Button btnCerrar;
+    private ImageView imgPerfil;
+    private Button btnCerrar;
+    private ImageView btnCamara;
+
+    final int COD_FOTO = 20;
+    final String CARPETA_RAIZ = "MisFotosApp";
+    final String CARPETA_IMAGENES = "imagenes";
+    final String RUTA_IMAGEN = CARPETA_RAIZ + CARPETA_IMAGENES;
+    String path;
 
 
     public PerfilFragment() {
@@ -34,7 +62,25 @@ public class PerfilFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        imgPerfil = view.findViewById(R.id.imgPerfil_fragmentPerfil);
         btnCerrar = view.findViewById(R.id.btnCerrarSesionUsuario);
+        btnCamara = view.findViewById(R.id.iconcamera_fragmenPerfil);
+
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TomarFoto();
+            }
+        });
+
+
+
 
         btnCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +95,59 @@ public class PerfilFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public void TomarFoto() {
+        String nombreImagen = "";
+
+        File fileImagen = new File(Environment.getExternalStorageDirectory(), RUTA_IMAGEN);
+        boolean isCreada = fileImagen.exists();
+
+        if(isCreada == false) {
+            isCreada = fileImagen.mkdirs();
+        }
+
+        if(isCreada == true) {
+            nombreImagen = (System.currentTimeMillis() / 1000) + ".jpg";
+        }
+
+        path = Environment.getExternalStorageDirectory()+File.separator+RUTA_IMAGEN+File.separator+nombreImagen;
+        File imagen = new File(path);
+
+        Intent intent = null;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            String authorities = this.getContext().getPackageName()+".FlieProvider";
+
+            Uri imageUri = FileProvider.getUriForFile(this.getActivity(), BuildConfig.APPLICATION_ID + ".FileProvider", imagen); android:authorities="${applicationId}.fileprovider";
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        } else {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
+        }
+        startActivityForResult(intent, COD_FOTO);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case COD_FOTO:
+                    MediaScannerConnection.scanFile(getContext(), new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                        @Override
+                        public void onScanCompleted(String path, Uri uri) {
+
+                        }
+                    });
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(path);
+                    imgPerfil.setImageBitmap(bitmap);
+
+                    break;
+            }
+        }
     }
 
 }
